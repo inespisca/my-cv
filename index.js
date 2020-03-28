e6a1081f2105aaf6a0bc7f87788ad71aacdac5de
Inês Oliveira <inesfpoliveira@gmail.com>
2020-03-28 23:57:21 +0000

    collapse function added to sidebar

---
+++ index.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sideNav").style.width = "280px";
+    document.getElementById("main").style.marginRight = "280px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  } 
